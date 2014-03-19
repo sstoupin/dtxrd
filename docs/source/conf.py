9764576c82123910f7c155cde79cf71d56de4aad
sstoupin <sstoupin@users.noreply.github.com>
2014-03-19 20:22:14 +0000

    ignore matplotlib in the docs for now

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 
 import sys
 import os
-from matplotlib.cm import spec
+#from matplotlib.cm import spec
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
